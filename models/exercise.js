0df8c1905580d2590148d6b37c96dad5af46ff4a
Elise_LaBonte <emlabonte714@gmail.com>
2021-10-23 13:32:00 -0400

    updated classes

---
+++ models/exercise.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise name."
+    }
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
